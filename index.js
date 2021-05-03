d8f659e1af00922f1d746374fe2a9bdb9b093626
JIAQI13 <jiaqi11@ualberta.ca>
2021-05-03 03:39:31 +0000

    finished

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  array.forEach(item=>{
+    Array.isArray(item) ? sum += sumItems(item) : sum+=item;
+  })
+  return sum;
 }
 
 module.exports = sumItems;
